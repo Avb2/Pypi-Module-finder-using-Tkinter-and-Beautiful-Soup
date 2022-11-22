058124ddc55fab5b13ccb7c2fea27fc9974837e2
avb2 <bringuel.alexander@gmail.com>
2022-11-22 03:05:23 -0500

    first commit

--- Module finder.py
+++ Module finder.py
@@ -18,35 +18,33 @@ def SearchIt():  # sourcery skip: hoist-statement-from-if
     search = SearchBox.get()
     url += search
 
-    # Run requests and scrape for the pip command
+    # Run requests and scrapes for the pip command
     req = requests.get(url)
     result = BeautifulSoup(req.text, 'html.parser')
-
-    #New
-    # pip command
     try:
         pip_command = result.find('span', {'id': 'pip-command'})
         found = pip_command.text
         print(found)
+        # If command is found, it will be displayed
         display = Label(root, text=found)
         display.grid(row=1, column=1)
 
         # Adds the pip command to clipboard
-        copy_to_clipboard = pc.copy(found)
+        pc.copy(found)
 # If the user search can't be found
     except Exception:
-        cant_find = ('Sorry I couldnt find that.')
+        cant_find = 'Sorry I couldnt find that.'
         print(cant_find)
         display = Label(root, text=cant_find)
         display.grid(row=1, column=1)
 
-
+# Clears the label that displays the pip command
 def clear_text():
     display = Label(root, text='            ')
     display.grid(row=1, column=1)
 
 
-# Enter UI
+# Enter button
 Enter = Button(root, text='enter', command=lambda: SearchIt())
 Enter.grid(row=0, column=0)
 
